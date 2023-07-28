62ead2fe1b78c114bc5c02d67e2e355d2d1b3a30
yehoki <patsenn2000@gmail.com>
2023-07-29 00:29:10 +0100

    Fix responsiveness on small screens

--- ps-portfolio/components/Projects/AllProjects/AllProjectItem.tsx
+++ ps-portfolio/components/Projects/AllProjects/AllProjectItem.tsx
@@ -24,7 +24,7 @@ const AllProjectItem: React.FC<AllProjectItemProps> = ({
 }) => {
   return (
     <div
-      className="h-full w-3/4 sm:w-2/3 md:w-7/12 lg:w-1/2 xl:w-2/5 2xl:w-1/3 
+      className="h-full w-3/4 2xl:max-w-1/3 sm:w-2/3 md:w-7/12 lg:w-1/2 xl:w-2/5 2xl:w-1/3
       flex flex-col flex-shrink-0
       py-4 sm:py-8 px-6 sm:px-10
       lg:hover:bg-psText/5 
@@ -34,7 +34,7 @@ const AllProjectItem: React.FC<AllProjectItemProps> = ({
       "
     >
       <div
-        className="w-full h-40 sm:h-48 md:h-56 lg:h-64 
+        className="w-full h-40 sm:h-48 md:h-56 lg:h-64
       rounded-lg mb-4 relative"
       >
         <Image
@@ -44,37 +44,39 @@ const AllProjectItem: React.FC<AllProjectItemProps> = ({
           className="pointer-events-none rounded-lg"
         />
       </div>
-      <div className="flex justify-between">
-        <div>
-          <div className="text-2xl mb-2">{title}</div>
+      <div className="">
+        <div className="">
+          <div className="flex justify-between gap-8 mb-2">
+            <div className="text-2xl ">{title}</div>
+            <div className="flex gap-2">
+              <a
+                target="_blank"
+                href={`${
+                  githubLink !== ''
+                    ? githubLink
+                    : 'https://github.com/yehoki/bookmarkt'
+                }`}
+              >
+                <AiFillGithub size={32} className="cursor-pointer" />
+              </a>
+              <a
+                target="_blank"
+                href={`${
+                  websiteLink !== ''
+                    ? websiteLink
+                    : 'https://github.com/yehoki/bookmarkt'
+                }`}
+              >
+                <TbWorld size={32} className="cursor-pointer" />
+              </a>
+            </div>
+          </div>
           <div className="text-sm text-psText/70 mb-2">{description}</div>
-          <div className="flex gap-2 items-center">
+          <div className="flex gap-2 items-center flex-wrap">
             {skills.map((skill) => (
               <SkillBox key={skill} skillTitle={skill} />
             ))}
           </div>
-        </div>
-        <div className="flex gap-2">
-          <a
-            target="_blank"
-            href={`${
-              githubLink !== ''
-                ? githubLink
-                : 'https://github.com/yehoki/bookmarkt'
-            }`}
-          >
-            <AiFillGithub size={32} className="cursor-pointer" />
-          </a>
-          <a
-            target="_blank"
-            href={`${
-              websiteLink !== ''
-                ? websiteLink
-                : 'https://github.com/yehoki/bookmarkt'
-            }`}
-          >
-            <TbWorld size={32} className="cursor-pointer" />
-          </a>
         </div>
       </div>
     </div>
